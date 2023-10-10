611ebc492b7f3eacfad17c1b1dee3d6538779ef5
AndrewGF90s <galchok87@gmail.com>
2023-10-10 20:39:50 +0300

    Viz

---
+++ Vizit/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("**************************");
+        System.out.println("****     Визитка      ****");
+        System.out.println("***    Name:\"Андрей\"   ***");
+        System.out.println("**** Scype : My Scype ****");
+        System.out.println("**************************");
+    }
+}
